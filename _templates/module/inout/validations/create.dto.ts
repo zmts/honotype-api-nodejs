90ce8cfc37d91dd8f75b548fce6495a0245a1b21
Sasha Zmts <sa.zmts@gmail.com>
2026-02-21 12:45:14 +0200

    templates

--- _templates/module/inout/validations/create.dto.ts
+++ _templates/module/inout/validations/create.dto.ts
@@ -1,7 +1,9 @@
-import { z, ZodType } from 'zod';
+import { ZodType } from 'zod';
 
 import { zodSchema } from '@libs/common/api';
 import { BaseValidator } from '@libs/core';
+
+import { schema } from './schema';
 
 export interface CreateDto {
   name: string;
@@ -10,7 +12,7 @@ export interface CreateDto {
 export class CreateDto extends BaseValidator<CreateDto> {
   protected schema(): ZodType {
     return zodSchema<CreateDto>({
-      name: z.string(),
+      name: schema.name,
     });
   }
 }
---
+++ _templates/module/inout/validations/schema.ts
@@ -0,0 +1,5 @@
+import { z } from 'zod';
+
+export const schema = {
+  name: z.string(),
+};
